fc92fe2b11a3aaba12dca5e22a4c5c1040d485fb
isebasi <isebasi@hotmail.com>
2020-05-13 19:35:57 -0500

    @

---
+++ database/migrations/2020_05_13_030814_create_invoices_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('expedition_date');
+            $table->date('expiration_date');
+            $table->date('receipt_date');
+            $table->string('payment_method');
+            $table->integer('quantity');
+            $table->decimal('subtotal');
+            $table->decimal('VAT');
+            $table->decimal('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}

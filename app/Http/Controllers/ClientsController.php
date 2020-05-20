3aec805e6a3e0f60d5e9655c6891aa8e74746896
Joan Sebastian Baron <isebasi@hotmail.com>
2020-05-19 22:25:01 -0500

    configurate botons and controlator

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Client;
 use Illuminate\Http\Request;
 
 class ClientsController extends Controller
@@ -13,7 +14,7 @@ class ClientsController extends Controller
      */
     public function index()
     {
-        return response()->view('clients.index');
+        return response()->view('clients.index',['clients'=> Client::all()]);
     }
 
     /**

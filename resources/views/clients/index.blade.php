22f13d5e364d0852645d74b234f0ac19bcc70311
isebasi <isebasi@hotmail.com>
2020-05-19 22:25:01 -0500

    configurate botons and controlator

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -25,68 +25,17 @@
                                 </div>
                             </form>
                             <div class="mdl-list">
+                                @foreach($clients as $client)
                                 <div class="mdl-list__item mdl-list__item--two-line">
 										<span class="mdl-list__item-primary-content">
 											<i class="zmdi zmdi-account mdl-list__item-avatar"></i>
-											<span>1. Client name</span>
+											<span>1. {{$client->title}}</span>
 											<span class="mdl-list__item-sub-title">DNI</span>
 										</span>
                                     <a class="mdl-list__item-secondary-action" href="#!"><i class="zmdi zmdi-more"></i></a>
                                 </div>
-                                <li class="full-width divider-menu-h"></li>
-                                <div class="mdl-list__item mdl-list__item--two-line">
-										<span class="mdl-list__item-primary-content">
-											<i class="zmdi zmdi-account mdl-list__item-avatar"></i>
-											<span>2. Client name</span>
-											<span class="mdl-list__item-sub-title">DNI</span>
-										</span>
-                                    <a class="mdl-list__item-secondary-action" href="#!"><i class="zmdi zmdi-more"></i></a>
-                                </div>
-                                <li class="full-width divider-menu-h"></li>
-                                <div class="mdl-list__item mdl-list__item--two-line">
-										<span class="mdl-list__item-primary-content">
-											<i class="zmdi zmdi-account mdl-list__item-avatar"></i>
-											<span>3. Client name</span>
-											<span class="mdl-list__item-sub-title">DNI</span>
-										</span>
-                                    <a class="mdl-list__item-secondary-action" href="#!"><i class="zmdi zmdi-more"></i></a>
-                                </div>
-                                <li class="full-width divider-menu-h"></li>
-                                <div class="mdl-list__item mdl-list__item--two-line">
-										<span class="mdl-list__item-primary-content">
-											<i class="zmdi zmdi-account mdl-list__item-avatar"></i>
-											<span>4. Client name</span>
-											<span class="mdl-list__item-sub-title">DNI</span>
-										</span>
-                                    <a class="mdl-list__item-secondary-action" href="#!"><i class="zmdi zmdi-more"></i></a>
-                                </div>
-                                <li class="full-width divider-menu-h"></li>
-                                <div class="mdl-list__item mdl-list__item--two-line">
-										<span class="mdl-list__item-primary-content">
-											<i class="zmdi zmdi-account mdl-list__item-avatar"></i>
-											<span>5. Client name</span>
-											<span class="mdl-list__item-sub-title">DNI</span>
-										</span>
-                                    <a class="mdl-list__item-secondary-action" href="#!"><i class="zmdi zmdi-more"></i></a>
-                                </div>
-                                <li class="full-width divider-menu-h"></li>
-                                <div class="mdl-list__item mdl-list__item--two-line">
-										<span class="mdl-list__item-primary-content">
-											<i class="zmdi zmdi-account mdl-list__item-avatar"></i>
-											<span>6. Client name</span>
-											<span class="mdl-list__item-sub-title">DNI</span>
-										</span>
-                                    <a class="mdl-list__item-secondary-action" href="#!"><i class="zmdi zmdi-more"></i></a>
-                                </div>
-                                <li class="full-width divider-menu-h"></li>
-                                <div class="mdl-list__item mdl-list__item--two-line">
-										<span class="mdl-list__item-primary-content">
-											<i class="zmdi zmdi-account mdl-list__item-avatar"></i>
-											<span>7. Client name</span>
-											<span class="mdl-list__item-sub-title">DNI</span>
-										</span>
-                                    <a class="mdl-list__item-secondary-action" href="#!"><i class="zmdi zmdi-more"></i></a>
-                                </div>
+                                    <li class="full-width divider-menu-h"></li>
+                                @endforeach
                             </div>
                         </div>
                     </div>
@@ -102,7 +51,7 @@
                             New client
                         </div>
                         <div class="full-width panel-content">
-                            <form>
+                            <form action="{{ route('clients.store') }}"  method="POST">
                                 <h5 class="text-condensedLight">Data client</h5>
                                 <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                                     <input class="mdl-textfield__input" type="text" pattern="-?[A-Za-záéíóúÁÉÍÓÚ ]*(\.[0-9]+)?" id="name">
@@ -141,10 +90,10 @@
                                 </div>
 
                                 <p class="text-center">
-                                    <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored bg-primary" id="btn-addClient">
-                                        <i class="zmdi zmdi-plus"></i>
+                                    <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                                        Button
                                     </button>
-                                <div class="mdl-tooltip" for="btn-addClient">Add client</div>
+                                `   <div class="mdl-tooltip" for="btn-addClient">Add client</div>
                                 </p>
                             </form>
                         </div>

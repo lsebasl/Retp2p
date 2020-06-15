d35434898a46b854a8b1058dd1330cc59965aca5
isebasi <isebasi@hotmail.com>
2020-06-15 17:57:21 -0500

    refactor controller-store

---
+++ app/Http/Controllers/Store/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Store;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return View
+     */
+    public function index():View
+    {
+        return view('store.home');
+    }
+}

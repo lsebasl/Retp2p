a9d298c09588d2fae2aeef4a822a5e4b520fe712
Sebastian <65204798+lsebasl@users.noreply.github.com>
2020-12-06 16:31:25 -0500

    feat: Create seeder to invoices.

--- database/seeds/InvoiceSeeder.php
+++ database/seeds/InvoiceSeeder.php
@@ -13,5 +13,20 @@ class InvoiceSeeder extends Seeder
     public function run()
     {
         factory(Invoice::class, 40)->create();
+
+        foreach (App\Invoice::all() as $invoice) {
+            foreach (App\Product::all() as $product) {
+
+                if (rand(1, 100) > 40) {
+                    $invoice->products()->attach(
+                        $product->id,[
+                        'product_id' => $product->id,
+                        'quantity' => 1,
+                        'total_by_product' => ($product->price * rand(1,100)),
+                        ]);
+                }
+            }
+            $invoice->save();
+        }
     }
 }
